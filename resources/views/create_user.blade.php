47e24d414a19d483f3ee5e77fbe5e834b74dd131
Muhammad Hildan Alfaris <hildanalfaris@gmail.com>
2024-10-04 21:10:36 +0700

    pembaruan-lima

--- resources/views/create_user.blade.php
+++ resources/views/create_user.blade.php
@@ -62,14 +62,31 @@
     </style>
 </head>
 <body>
+@extends('layouts.app')
+
+@section('content')
 <div class="container">
+<h1>Create User</h1>
     <form action="/user/store" method="POST">
-        @include('_form', [
-            'action' => route('user.store'),
-            'kelas' => $kelas,
-            ])
-    
+        @csrf
+        <label for="nama">Nama:</label>
+        <input type="text" id="nama" name="nama" required>
+
+        <label for="npm">NPM:</label>
+        <input type="text" id="npm" name="npm" required>
+
+        <label for="kelas_id">Kelas:</label>
+        <select id="kelas_id" name="kelas_id" required>
+            <!-- Menambahkan opsi default untuk tidak memilih kelas A -->
+            <option value="" disabled selected>Pilih Kelas</option>
+            @foreach($kelas as $kelasItem)
+                <option value="{{ $kelasItem->id }}">{{ $kelasItem->nama_kelas }}</option>
+            @endforeach
+        </select>
+
+        <button type="submit">Submit</button>
     </form>
 </div>
+@endsection
 </body>
 </html>
